6b4e47e4576effc0ebf4aa1cf9efb5d8ab2c0bbc
C6H5-NO2 <19368807+C6H5-NO2@users.noreply.github.com>
2023-12-12 01:53:34 +0100

    add cuckoo strategy

---
+++ probfilter-core/src/main/scala/probfilter/pdsa/CuckooStrategy.scala
@@ -0,0 +1,34 @@
+package probfilter.pdsa
+
+import probfilter.pdsa.CuckooStrategy.CuckooPair
+import probfilter.pdsa.ScalaFunnels.ByteFunnel
+
+
+@SerialVersionUID(1L)
+class CuckooStrategy[T](
+  val capacity: Int,
+  val bucketSize: Int,
+  val maxIterations: Int
+)(implicit val funnel: Funnel[_ >: T]) extends Serializable {
+  def getCuckooPair(elem: T): CuckooPair = {
+    val hash = MurmurHash3.hash(elem)
+    val fp = (hash % 255 + 1).toByte
+    CuckooPair(fp, (hash & Int.MaxValue).toInt % capacity)
+  }
+
+  def getAltBucket(pair: CuckooPair): Int = getAltBucket(pair.fp, pair.i)
+
+  def getAltBucket(fp: Byte, i: Int): Int = {
+    (i ^ (MurmurHash3.hash(fp) & Int.MaxValue).toInt) % capacity
+  }
+
+  def fpp: Double = {
+    val p = 8
+    bucketSize * 2.0 / (1 << p)
+  }
+}
+
+
+object CuckooStrategy {
+  case class CuckooPair(fp: Byte, i: Int)
+}
