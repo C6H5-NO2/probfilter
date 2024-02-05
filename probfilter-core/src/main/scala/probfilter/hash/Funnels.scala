b0c7d513904ba19be7b1c512f07553bfb2606d5b
C6H5-NO2 <19368807+C6H5-NO2@users.noreply.github.com>
2024-02-05 18:49:04 +0100

    add default funnel

--- probfilter-core/src/main/scala/probfilter/hash/Funnels.scala
+++ probfilter-core/src/main/scala/probfilter/hash/Funnels.scala
@@ -2,12 +2,11 @@ package probfilter.hash
 
 import probfilter.util.JavaFriendly
 
+import java.io.{ByteArrayOutputStream, ObjectOutputStream}
 import java.nio.charset.StandardCharsets
 
 
-/**
- * Common [[probfilter.hash.Funnel]]s.
- */
+/** Common [[probfilter.hash.Funnel]]s. */
 object Funnels {
   @SerialVersionUID(1L)
   implicit final object StringFunnel extends Funnel[String] {
@@ -24,9 +23,23 @@ object Funnels {
     override def funnel(from: Integer, into: Sink): Unit = into.putInt(from)
   }
 
+  @SerialVersionUID(1L)
+  /* implicit */ final object DefaultFunnel extends Funnel[Object] {
+    override def funnel(from: Object, into: Sink): Unit = {
+      val bos = new ByteArrayOutputStream()
+      val oos = new ObjectOutputStream(bos)
+      oos.writeObject(from)
+      oos.close()
+      into.putBytes(bos.toByteArray)
+    }
+  }
+
   @JavaFriendly(scalaDelegate = "probfilter.hash.Funnels.StringFunnel")
   def getStringFunnel: Funnel[String] = StringFunnel
 
   @JavaFriendly(scalaDelegate = "probfilter.hash.Funnels.IntFunnel")
   def getIntegerFunnel: Funnel[Integer] = IntegerFunnel
+
+  @JavaFriendly(scalaDelegate = "probfilter.hash.Funnels.DefaultFunnel")
+  def getDefaultFunnel: Funnel[Object] = DefaultFunnel
 }
