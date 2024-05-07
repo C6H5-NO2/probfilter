3d942db34001b36d601a0653490451d5130daa68
C6H5-NO2 <19368807+C6H5-NO2@users.noreply.github.com>
2024-05-07 02:56:09 +0200

    add mutable grow-only bloom filter

---
+++ probfilter-core/src/main/scala/probfilter/crdt/mutable/GBloomFilter.scala
@@ -0,0 +1,29 @@
+package probfilter.crdt.mutable
+
+import probfilter.crdt.Convergent
+import probfilter.pdsa.Filter
+import probfilter.pdsa.bloom.BloomStrategy
+import probfilter.pdsa.bloom.mutable.BloomFilter
+
+
+/** A mutable grow-only replicated bloom filter. */
+@SerialVersionUID(1L)
+final class GBloomFilter[E] private(private val state: BloomFilter[E]) extends Filter[E] with Convergent[GBloomFilter[E]] {
+  def this(strategy: BloomStrategy[E]) = this(new BloomFilter[E](strategy))
+
+  def strategy: BloomStrategy[E] = state.strategy
+
+  override def size(): Int = state.size()
+
+  override def capacity(): Int = state.capacity()
+
+  override def fpp(): Double = state.fpp()
+
+  override def contains(elem: E): Boolean = state.contains(elem)
+
+  override def add(elem: E): GBloomFilter[E] = {state.add(elem); this}
+
+  override def merge(that: GBloomFilter[E]): GBloomFilter[E] = {this.state.union(that.state); this}
+
+  override def toString: String = s"GBF($state)"
+}
