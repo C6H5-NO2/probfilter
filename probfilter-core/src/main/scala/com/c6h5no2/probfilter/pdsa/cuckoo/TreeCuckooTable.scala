d63ea0eb71a4a9085aea75b63f56b410886f348b
C6H5-NO2 <19368807+C6H5-NO2@users.noreply.github.com>
2024-10-29 12:13:37 +0800

    add tostring to vector based cuckoo table

--- probfilter-core/src/main/scala/com/c6h5no2/probfilter/pdsa/cuckoo/TreeCuckooTable.scala
+++ probfilter-core/src/main/scala/com/c6h5no2/probfilter/pdsa/cuckoo/TreeCuckooTable.scala
@@ -1,7 +1,9 @@
 package com.c6h5no2.probfilter.pdsa.cuckoo
 
 import com.c6h5no2.probfilter.util.ArrayOpsEx
+import com.c6h5no2.probfilter.util.ClassEx.Clazz
 
+import scala.collection.AbstractIterator
 import scala.reflect.{ClassTag, classTag}
 
 
@@ -9,9 +11,10 @@ import scala.reflect.{ClassTag, classTag}
  * A tree-based solution for immutable cuckoo table in upcoming versions.
  * Currently use Vector as a place holder.
  */
+@SerialVersionUID(1L)
 final class TreeCuckooTable[T: ClassTag] private(
-  private[this] val data: Vector[T],
-  private[this] val overflowed: MapCuckooTable[T],
+  data: Vector[T],
+  overflowed: MapCuckooTable[T],
   val numBuckets: Int,
   val bucketSize: Int,
   val size: Int,
@@ -43,5 +46,28 @@ final class TreeCuckooTable[T: ClassTag] private(
     }
     val newOverflowed = overflowed.set(index, value.drop(bucketSize))
     new TreeCuckooTable[T](newData, newOverflowed, index + 1, bucketSize, newSize)
+  }
+
+  override def toString: String = {
+    new TreeCuckooTable.TreeCuckooTableIterator(this)
+      .zipWithIndex
+      .filter(_._1.nonEmpty)
+      .map(tup => tup._1.mkString(s"${tup._2}->[", ", ", "]"))
+      .mkString(s"${getClass.getShortName}{", ", ", "}")
+  }
+}
+
+object TreeCuckooTable {
+  private final class TreeCuckooTableIterator[T](
+    table: TreeCuckooTable[T],
+  ) extends AbstractIterator[Array[T]] {
+    private[this] var iter = 0
+
+    override def hasNext: Boolean = iter < table.numBuckets
+
+    override def next(): Array[T] = {
+      iter += 1
+      table.get(iter - 1)
+    }
   }
 }
