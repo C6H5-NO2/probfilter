b33a0cd1d343dcc25361db0563a1d436fa447593
C6H5-NO2 <19368807+C6H5-NO2@users.noreply.github.com>
2024-10-29 12:05:22 +0800

    add vector based cuckoo table

---
+++ probfilter-core/src/main/scala/com/c6h5no2/probfilter/pdsa/cuckoo/TreeCuckooTable.scala
@@ -0,0 +1,47 @@
+package com.c6h5no2.probfilter.pdsa.cuckoo
+
+import com.c6h5no2.probfilter.util.ArrayOpsEx
+
+import scala.reflect.{ClassTag, classTag}
+
+
+/**
+ * A tree-based solution for immutable cuckoo table in upcoming versions.
+ * Currently use Vector as a place holder.
+ */
+final class TreeCuckooTable[T: ClassTag] private(
+  private[this] val data: Vector[T],
+  private[this] val overflowed: MapCuckooTable[T],
+  val numBuckets: Int,
+  val bucketSize: Int,
+  val size: Int,
+) extends TypedCuckooTable[T] {
+  def this(numBuckets: Int, bucketSize: Int) = this(
+    Vector.empty[T],
+    new MapCuckooTable.Immutable[T](bucketSize),
+    numBuckets,
+    bucketSize,
+    0
+  )
+
+  override def storageType: ClassTag[T] = classTag[T]
+
+  override def get(index: Int): Array[T] = {
+    val from = index * bucketSize
+    val d = data.slice(from, from + bucketSize).filter(_ != 0L.asInstanceOf[T]).toArray[T]
+    val o = overflowed.get(index)
+    ArrayOpsEx.concated(d, o)
+  }
+
+  override def set(index: Int, value: Array[T]): TypedCuckooTable[T] = {
+    val newSize = size - size(index) + value.length
+    var newData = if (index < data.length / bucketSize) data else data.padTo((index + 1) * bucketSize, 0L.asInstanceOf[T])
+    newData = newData.patch(index * bucketSize, value, math.min(bucketSize, value.length))
+    if (value.length < bucketSize) {
+      val zeros = ArrayOpsEx.zeros[T](bucketSize - value.length)
+      newData = newData.patch(index * bucketSize + value.length, zeros, zeros.length)
+    }
+    val newOverflowed = overflowed.set(index, value.drop(bucketSize))
+    new TreeCuckooTable[T](newData, newOverflowed, index + 1, bucketSize, newSize)
+  }
+}
